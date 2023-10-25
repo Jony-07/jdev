574a23a5f2f9d89b3f880ba6844f0bdc8fe838a9
Jony Morales <jony25lopezml@gmail.com>
2023-10-25 00:09:27 -0600

    details

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,29 +10,29 @@ const NavBar = () => {
   const links = [
     {   
         id:1,
-        link:'Home'
+        link:'home'
     },
     {   
         id:2,
-        link:'About'
+        link:'about'
     },
     {   
         id:3,
-        link:'Portfolio'
+        link:'portfolio'
     },
     {   
         id:4,
-        link:'Experience'
+        link:'experience'
     },
     {
         id:5,
-        link:'Contact'
+        link:'contact'
     }
   ]
     
   return (
-    <div className=' z-30 flex justify-between items-center w-full h-20
-    text-white fixed px-4 txt'>
+    <div className='fixed z-30 flex justify-between items-center w-full h-20
+    text-white  px-4 txt'>
     <div>
         <h1 className='text-5xl font-signature ml-2'>Jony <span >Dev</span> </h1>
     </div>
@@ -56,9 +56,9 @@ const NavBar = () => {
 
     {nav && (
         <ul className='flex flex-col justify-center items-center absolute
-    top-0 left-0 w-full h-screen coverMenu text-gray-500'>
+    top-0 left-0 w-full h-screen coverMenu text-gray-500 '>
          {links.map(({id,link})=>(
-        <li key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl '>
+        <li key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl  '>
             <Link
             onClick={() => setNav(!nav)} 
             to={link} 
